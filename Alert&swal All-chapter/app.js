dccc3ee3342bc15d22326954c7c5575eadf58ad4
AhmedRazaaAttari <154536166+AhmedRazaaAttari@users.noreply.github.com>
2024-02-27 13:39:30 +0500

    Add files via upload

---
+++ Alert&swal All-chapter/app.js
@@ -0,0 +1,60 @@
+// chapter-No-1
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+        alert("hello world!");
+
+        // chapter-No-2
+        // 2. Write a script to display following message on your web
+// page:
+
+        alert('Error! pleace enter a valid password.');
+
+        // chapter-No-3
+        // 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+        alert('Welcome to JS Land... Happy Coding!');
+
+        // chapter-no-4
+        // 4. Write a script to display following messages in sequence:
+
+        alert('Welcome to JS Land...');
+        alert('Happy Coding!/n Prevent this page from creating additional dialogs.');
+
+        // chapter-No-5
+        // 5. Generate the following message through browser’s
+// developer console:
+
+        alert("hello... I can run JS through my web browser's console");
+
+        // chapter-No-1
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+        swal("hello world!");
+
+        
+        // chapter-No-2
+        // 2. Write a script to display following message on your web
+// page:
+
+        swal('Error! pleace enter a valid password.');
+
+// chapter-No-3
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+        swal('Welcome to JS Land... Happy Coding!');
+
+// chapter-no-4
+// 4. Write a script to display following messages in sequence:
+
+        swal('Welcome to JS Land...');
+        swal('Happy Coding!/n Prevent this page from creating additional dialogs.');
+
+// chapter-No-5
+// 5. Generate the following message through browser’s
+// developer console:
+
+        // swal("hello... I can run JS through my web browser's console");
